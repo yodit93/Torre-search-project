981ae166b1bd1d0baabe66736dbcb76b8879c37b
yodit93 <yoditabebe9391@gmail.com>
2023-09-18 15:24:53 +0300

    Create search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+const Search = () => {
+    return (
+        <div className="serach-cont">
+             <input type="text" placeholder="Search by name"/>
+        </div>   
+     );
+}
+ 
+export default Search;
