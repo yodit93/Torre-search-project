af133515a689fee815becd20511c8d1703927b6f
yodit93 <yoditabebe9391@gmail.com>
2023-09-18 15:25:06 +0300

    Create Navigation coponent

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+const Navigation = () => {
+    return ( 
+        <nav className="nav">
+            <span className="logo">SEARCH</span>
+            <ul className="nav-links">
+                <li>BY NAME</li>
+            </ul>
+        </nav>
+    );
+}
+ 
+export default Navigation;
