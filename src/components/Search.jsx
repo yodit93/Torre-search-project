8b1ced2ae00115b60d06a27392b097d3580614d9
yodit93 <yoditabebe9391@gmail.com>
2023-09-18 22:06:36 +0300

    Fetch & use candidates from store

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,46 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getCandidates } from "../Redux/candidatesSlice";
+import { useEffect, useState } from "react";
+import Candidate from "./Candidate";
+
 const Search = () => {
+     const dispatch = useDispatch();
+     const {candidates} = useSelector((state) => state.candidates);
+     const [state, setState] = useState({
+          searchValue: '',
+          filtered: [],
+     });
+     useEffect(() => {
+       dispatch(getCandidates());
+     },[dispatch]);
+     const handleSearch = (e) => {
+          const results = candidates.filter((candidate) => {
+               if(e.target.value === '') return;
+               return candidate.name.toLowerCase().includes(e.target.value.toLowerCase());
+          });
+          setState({
+               searchValue: e.target.value,
+               filtered: results,
+          })
+     }
+     console.log(state.filtered);
     return (
-        <div className="serach-cont">
-             <input type="text" placeholder="Search by name"/>
-        </div>   
+     <>
+          <div className="search-cont">
+             <input
+             type="text"
+             placeholder="Search by name"
+             value={state.searchValue}
+             onChange={handleSearch}
+             />
+          </div>
+          <div className="filtered-cont">
+               {state.filtered && state.filtered.map((candidate) => {
+               return (
+                  <Candidate key={candidate.id} candidate={candidate}/>  
+               )})}
+          </div>
+     </>
      );
 }
  
